35af5cd09651e7db565d745e626dc4d6d377ec55
Zach Overhulser <zoverhulser@gmail.com>
2017-08-26 17:11:42 -0400

    All tests pass

--- leap.test.js
+++ leap.test.js
@@ -5,3 +5,28 @@ test("a year that is not a leap year", function(){
 	store.dispatch(changeYear(2015));
 	expect(store.getState().isLeap).toBe(false);
 });
+
+test("a year that is a leap year", function(){
+store.dispatch(changeYear(2016));
+expect(store.getState().isLeap).toBe(true);
+});
+
+test("a year that is not a leap year", function(){
+    store.dispatch(changeYear(2011));
+    expect(store.getState().isLeap).toBe(false);
+});
+
+test("another year that is a leap year", function(){
+    store.dispatch(changeYear(2012));
+    expect(store.getState().isLeap).toBe(true);
+});
+
+test("every one hundred years we skip a leap year", function(){
+    store.dispatch(changeYear(1900));
+    expect(store.getState().isLeap).toBe(false);
+});
+
+test("every four hundred years we bring the leap year back", function(){
+    store.dispatch(changeYear(2000));
+    expect(store.getState().isLeap).toBe(true);
+});
--- reducers.js
+++ reducers.js
@@ -1,3 +1,23 @@
-module.exports.checkLeapYear = function(state = {}, action){
-	return state
+module.exports.checkLeapYear = function(state = {isLeap: false}, action){
+    if(action.year % 4 === 0){
+        if(action.year % 100 === 0){
+	  if(action.year % 400 === 0){
+	      return {
+	              isLeap:true
+		     }
+	  
+	  }
+	    return {
+	            isLeap:false
+	           }
+	
+	}
+	return {
+        	isLeap:true
+	       }
+	                     }
+
+    return {
+    	      isLeap:false
+	   }
 };
